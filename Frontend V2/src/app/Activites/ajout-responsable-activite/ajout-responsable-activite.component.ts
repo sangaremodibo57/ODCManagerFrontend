fbb6134dbabae4ac693ec3aeddedf67060a60e0b
SeydinaOumarDiarra <seydinaoumardiarra@gmail.com>
2021-12-01 19:59:11 +0000

    Ajout partie contrainte sur le responsable

--- Frontend V2/src/app/Activites/ajout-responsable-activite/ajout-responsable-activite.component.ts
+++ Frontend V2/src/app/Activites/ajout-responsable-activite/ajout-responsable-activite.component.ts
@@ -16,7 +16,11 @@ export class AjoutResponsableActiviteComponent implements OnInit {
   responsable: any;
   logActivite: any;
   responsableParActivite: any;
+  responsableParRes: any;
   errorEmailResp: any;
+  errorEmailResp1: any;
+  respExistant: any;
+  res: any;
 
   constructor(
     private service: ActiviteServiceService,
@@ -33,7 +37,11 @@ export class AjoutResponsableActiviteComponent implements OnInit {
 
     this.service.ResponsableParActivite(this.id).subscribe((data:any)=>{
       this.responsableParActivite = data;
-      console.log(this.responsableParActivite);
+    });
+
+    this.serviceResponsable.listeResponsable().subscribe((data: any)=>{
+        this.responsableParRes = data;
+          console.log(this.responsableParRes);
     })
   }
 
@@ -47,19 +55,49 @@ export class AjoutResponsableActiviteComponent implements OnInit {
                     "etat": "active",
                     "type": form.value['type'],
                   };
-  for(let i=0; i<this.responsableParActivite.length; i++){
-    if(this.responsableParActivite[i].activite.email == form.value['email']){
-      this.errorEmailResp = "Ce Responsable existe déjà !"
+
+  for(let i=0; i<this.responsableParActivite.length; i++){    
+    if(this.responsableParActivite[i].responsable.email == form.value['email']){
+      this.errorEmailResp = "Ce Responsable est déjà affecté à cet activité !"
+      console.log(this.errorEmailResp);
     }
   }
-      // this.serviceResponsable.ajoutResponsale(this.respons).subscribe((data:any)=>{
-      // this.responsable = data;
-      //   this.logActivite = {"responsable": this.responsable, "activite": this.activite}
-      //   this.service.AjoutLog(this.logActivite).subscribe((log: any)=>{
-      //     this.router.navigate(['liste-activite'])
-      //   })
+
+  for(let i=0; i<this.responsableParRes.length; i++){    
+    if(this.responsableParRes[i].email == form.value['email']){
+      this.errorEmailResp1 = "Ce Responsable existe déjà !";
+      this.serviceResponsable.detailResponsable(this.responsableParRes[i].id_responsable).subscribe((data: any)=>{
+        console.log(data);
+        this.respExistant = data;
+      })
       
-      // });
+    }
+  }
+  
+  if(this.errorEmailResp1 != ''){
+    if(this.errorEmailResp != ''){
+      this.serviceResponsable.ajoutResponsale(this.respons).subscribe((data:any)=>{
+        this.responsable = data;
+          this.logActivite = {"responsable": this.responsable, "activite": this.activite}
+          this.service.AjoutLog(this.logActivite).subscribe((log: any)=>{
+            this.router.navigate(['liste-activite'])
+          })
+        });
+    }else{
+      this.errorEmailResp = "Ce responsable est déjà affecté à cet activité !"
+    }
+  }else{
+    this.errorEmailResp1 = "Ce responsable existe déjà !";
+  }
+  }
+
+  affecterResp(data: any){
+    this.serviceResponsable.detailResponsable(data).subscribe((datas: any)=>{
+      this.res = datas;
+      console.log(this.res);
+    })
+    
+    
   }
 
 }
